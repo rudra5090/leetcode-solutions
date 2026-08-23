ff6e1b7b125638d1068c830a6f21d2497254641d
Rudranarayan Debata <rudranarayan133456@gmail.com>
2026-08-23 19:16:18 +0530

    Time: 333 ms (37.72%), Space: 19.1 MB (97.72%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans = ""
+
+        for i in range(len(s)):
+            # Odd length palindrome
+            left = right = i
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > len(ans):
+                    ans = s[left:right + 1]
+                left -= 1
+                right += 1
+
+            # Even length palindrome
+            left = i
+            right = i + 1
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > len(ans):
+                    ans = s[left:right + 1]
+                left -= 1
+                right += 1
+
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
