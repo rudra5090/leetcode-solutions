c0d06f93a66969dad9fcdad4792fbac731acbd94
Rudranarayan Debata <rudranarayan133456@gmail.com>
2026-08-28 19:22:44 +0530

    Time: 3 ms (31.97%), Space: 19.2 MB (63.32%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for ch in s:
+            if ch == '(' or ch == '[' or ch == '{':
+                stack.append(ch)
+
+            else:
+                if not stack:
+                    return False
+
+                top = stack.pop()
+
+                if ch == ')' and top != '(':
+                    return False
+                if ch == ']' and top != '[':
+                    return False
+                if ch == '}' and top != '{':
+                    return False
+
+        return len(stack) == 0
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
